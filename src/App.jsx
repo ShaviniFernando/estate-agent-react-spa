50549e27861d8e7390683939aa90702b438a07fe
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-07-08 21:02:45 +0530

    Configure React Router and project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import PropertyDetails from './pages/PropertyDetails';
 import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/property/:id" element={<PropertyDetails />} />
+
+        <Route 
+          path="/property/:id" 
+          element={<PropertyDetails />} 
+        />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
