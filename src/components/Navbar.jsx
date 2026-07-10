dfe8776f58a0cb2d98495d3eec458eb09ea25cbb
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-07-10 13:13:01 +0530

    Implement Navbar component with link to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,9 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
-  return null;
+  return (
+    <header>
+      <Link to="/">Estate Agent</Link>
+    </header>
+  );
 }
