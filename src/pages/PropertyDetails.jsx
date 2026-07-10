fc426cbc7c7305744fd331f1fbc08ebe5e77d763
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-07-10 13:44:49 +0530

    Implement basic PropertyDetails component

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,11 +1,28 @@
 import { useParams } from 'react-router-dom';
+import properties from '../data/properties';
 
 export default function PropertyDetails() {
   const { id } = useParams();
+  const property = properties.find((p) => p.id === id);
+
+  if (!property) {
+    return (
+      <div>
+        <h1>Property not found</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Property Details</h1>
-      <p>Property ID: {id}</p>
+    <div className="property-details">
+      <h1>{property.location}</h1>
+      <img src={property.images[0]} alt={property.shortDescription} />
+      <div>
+        <h2>{property.type}</h2>
+        <h3>£{property.price.toLocaleString()}</h3>
+        <p>Bedrooms: {property.bedrooms}</p>
+        <p>{property.description}</p>
+      </div>
     </div>
   );
 }
