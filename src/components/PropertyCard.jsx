937c4c31400a7d230b39217eb7b1e19dfbcf5a82
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-07-10 13:13:04 +0530

    Implement PropertyCard component to display property details

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,3 +1,15 @@
-export default function PropertyCard() {
-  return null;
+import { Link } from 'react-router-dom';
+
+export default function PropertyCard({ property }) {
+  return (
+    <div className="property-card">
+      <img src={property.images[0]} alt={property.shortDescription} />
+      <div>
+        <p>{property.type} - {property.bedrooms} bed</p>
+        <h3>£{property.price.toLocaleString()}</h3>
+        <p>{property.shortDescription}</p>
+        <Link to={`/property/${property.id}`}>View details</Link>
+      </div>
+    </div>
+  );
 }
