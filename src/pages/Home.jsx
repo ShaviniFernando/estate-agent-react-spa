e7606231e5ad82b216ffdbb2fad58968290e0491
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-07-10 13:13:11 +0530

    Update Home page to render PropertyList

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import properties from '../data/properties';
+import PropertyList from '../components/PropertyList';
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <PropertyList properties={properties} />
     </div>
   );
 }
